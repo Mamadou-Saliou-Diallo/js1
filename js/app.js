bb46dbbd18764cf66b372156c9da161457819c65
Mamadou Saliou DIALLO <Mamadou-Saliou-DILLO>
2024-09-18 12:37:02 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Une ligne de commentaire
+
+/* Un commentaire sur 
+plusieeurs lignes */
+
+// Ouvrir une boite de dialogue
+//alert("Bonjours je suis le fichier javascript");
+// Text en console
+
+console.log("Text en console");
